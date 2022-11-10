1a43aa0ac32d667987bbfe1a0cae8f6f9ff56337
s4uc3 <heflerdev@gmail.com>
2022-11-10 16:30:10 -0300

    OBA - Create banks migration file

--- api/migrations/20221105005135_initial_migration.ts
+++ api/migrations/20221105005135_initial_migration.ts
@@ -8,11 +8,20 @@ export async function up(knex: Knex): Promise<void> {
         table.string("password")
         table.integer("email")
     })
-    // TODO: Create the Migration for the open_bank data
+    await knex.schema.createTable('banks',(table:Knex.TableBuilder) => {
+        table.increments('id').primary()
+        table.string("name")
+        table.uuid("user_id")
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onDelete("CASCADE")
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.dropTableIfExists('user')
+    await knex.schema.dropTableIfExists('bank')
 }
 
