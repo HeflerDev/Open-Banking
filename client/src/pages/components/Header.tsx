7c90605e591a91225ff694db8582e8bf0b2860b5
s4uc3 <heflerdev@gmail.com>
2022-11-14 20:15:09 -0300

    OBA - Public landing page on wip

--- client/src/pages/components/Header.tsx
+++ client/src/pages/components/Header.tsx
@@ -1,24 +1,23 @@
 import React from "react";
-import {Col, Container, Image, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 
 export const Header = () => {
     return (
         <div className={"header-container"}>
-           <Container>
-               <Row>
-                   <Col sm={12}>
-                       <div className="header-image"><Image className={"image"} fluid/></div>
-                       <div className="header-text">
-                           <h3 className="subtitle">
-                               Lorem Ipsum
-                           </h3>
-                           <p className="description">
-                               Lorem ipsum dolor sit amet nunquam
-                           </p>
-                       </div>
-                   </Col>
-               </Row>
-           </Container>
+            <Container>
+                <Row>
+                    <Col sm={12}>
+                        <div className="header-text">
+                            <h3 className="subtitle">
+                                Lorem Ipsum
+                            </h3>
+                            <p className="description">
+                                Lorem ipsum dolor sit amet nunquam
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
