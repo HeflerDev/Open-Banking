56c03af3156c0e449ebbdffd61c09ca408e18f95
s4uc3 <heflerdev@gmail.com>
2022-11-14 16:16:37 -0300

    OBA - Nav Component

---
+++ client/src/assets/icons/Icons.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Icon} from "@iconify/react";
+
+interface IconProps {
+    width?: number
+    height?: number
+    color?: string
+}
+
+export const IconUser = ({width = 20, height = 20, color = "black"}: IconProps) => <Icon width={width} height={height}
+                                                                                         color={color}
+                                                                                         icon="carbon:user-avatar-filled"/>
+export const IconHome = ({width = 20, height = 20, color = "black"}: IconProps) => <Icon width={width} height={height}
+                                                                                         color={color}
+                                                                                         icon="ant-design:home-filled"/>
+export const IconSrch = ({width = 20, height = 20, color = "black"}: IconProps) => <Icon width={width} height={height}
+                                                                                         color={color}
+                                                                                         icon="bi:search"/>
+export const IconDock = ({width = 20, height = 20, color = "black"}: IconProps) => <Icon width={width} height={height}
+                                                                                         color={color}
+                                                                                         icon="fluent-mdl2:docker-logo"/>
+export const IconConf = ({width = 20, height = 20, color = "black"}: IconProps) => <Icon width={width} height={height}
+                                                                                         color={color} icon={"fa:cog"}/>
--- client/src/pages/components/Navbar.tsx
+++ client/src/pages/components/Navbar.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import {Col, Row} from "react-bootstrap";
+import {IconConf, IconDock, IconHome, IconUser} from "../../assets/icons/Icons";
 
 export const Navbar = () => {
     return (
         <>
             <Row className="nav-container">
-                <Col className="logo-block"></Col>
+                <Col className="logo-block">
+                    <IconDock width={40} height={40} color={"#fff"}/>
+                    <div className="text">Open Bank</div>
+                </Col>
                 <Col className="user-block">
-                    <div className="user">User Logo</div>
                     <div className="buttons">
-                        <button "btn__primary"></button>
-                        <button "btn__primary"></button>
+                        <button className={"btn__secondary"}>Lorem</button>
+                        <button className={"btn__secondary"}>Ipsum</button>
                     </div>
                     <div className="icons">
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                        <div><IconHome color={"fff"}/></div>
+                        <div><IconConf color={"fff"}/></div>
+                        <div><IconUser width={40} height={40} color={"#fff"}/></div>
                     </div>
                 </Col>
             </Row>
